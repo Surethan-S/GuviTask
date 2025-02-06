65ac975aca1fffc7c29697855537b37712e467e3
Surethan-S <127739936+Surethan-S@users.noreply.github.com>
2025-02-06 15:14:15 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    echo hi123
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u surethan37 -p 55665566@S37
-    docker tag test surethan37/task2:sample
-    docker push surethan37/task2:sample
-    
