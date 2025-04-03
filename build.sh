2b2f48c0777d7c98de58a058eed7de031ee09ce7
Surethan-S <127739936+Surethan-S@users.noreply.github.com>
2025-04-03 10:57:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ docker login -u surethan37 -p 55665566@S37
 docker tag task2 surethan37/task4
 docker push surethan37/task4
 kubectl apply -f  deploy.yaml
-kubetcl apply -f svc.yaml
+kubectl apply -f svc.yaml
